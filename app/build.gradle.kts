8bea84b6dc7a0b903bb7024988995fbee2ff7dae
ushjdjsyudjd-rgb <ushjdjsyudjd@gmail.com>
2025-12-29 02:13:39 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,22 +4,52 @@ plugins {
 }
 
 android {
-    namespace 'com.example.photoeditor'
+    // این بخش با ساختار پوشه‌بندی پروژه‌ای که فورک کردی هماهنگ شد
+    namespace 'com.example.android.architecture.blueprints.todoapp' 
     compileSdk 34
 
     defaultConfig {
-        applicationId "com.example.photoeditor"
+        applicationId "com.example.android.architecture.blueprints.todoapp"
         minSdk 24
         targetSdk 34
         versionCode 1
         versionName "1.0"
+        
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary true
+        }
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_17
+        targetCompatibility JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = '17'
     }
 
     buildFeatures {
         compose true
     }
+    
     composeOptions {
+        // این نسخه باید با نسخه کاتلین پروژه هماهنگ باشد
         kotlinCompilerExtensionVersion '1.5.1'
+    }
+    
+    packagingOptions {
+        resources {
+            excludes += '/META-INF/{AL2.0,LGPL2.1}'
+        }
     }
 }
 
@@ -29,6 +59,10 @@ dependencies {
     implementation 'androidx.activity:activity-compose:1.8.2'
     implementation platform('androidx.compose:compose-bom:2023.08.0')
     implementation 'androidx.compose.ui:ui'
+    implementation 'androidx.compose.ui:ui-graphics'
+    implementation 'androidx.compose.ui:ui-tooling-preview'
     implementation 'androidx.compose.material3:material3'
+    
+    // کتابخانه لازم برای لود کردن عکس
     implementation "io.coil-kt:coil-compose:2.5.0"
 }
